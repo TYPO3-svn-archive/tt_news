7be5eca63b82534f5f0ff9199687153020ed3dd7
kasper <kasper@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-04-01 10:21:04 +0000

    Initial revision

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: 'tt_news'
+# 
+# Auto generated 12-02-2003 21:16
+# 
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'News',
+	'description' => 'Website news with front page teasers and article handling inside.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => 0,
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'private' => 0,
+	'download_password' => '',
+	'version' => '1.0.4',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:13:{s:12:"ext_icon.gif";s:4:"5e2a";s:14:"ext_tables.php";s:4:"0dca";s:14:"ext_tables.sql";s:4:"603b";s:28:"ext_typoscript_constants.txt";s:4:"05e7";s:24:"ext_typoscript_setup.txt";s:4:"0e4b";s:17:"locallang_tca.php";s:4:"5893";s:7:"tca.php";s:4:"b7a8";s:22:"pi/class.tx_ttnews.php";s:4:"4314";s:10:"pi/new.gif";s:4:"7f00";s:17:"pi/news_help.tmpl";s:4:"6121";s:17:"pi/news_help1.gif";s:4:"d8fb";s:21:"pi/news_template.tmpl";s:4:"5387";s:14:"doc/manual.sxw";s:4:"0735";}',
+);
+
+?>
