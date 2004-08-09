f6b305d55ecc0b1ebd8f2d2cd71412ea9ab747fb
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-08-09 18:19:46 +0000

    actual emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_news"
 # 
-# Auto generated 06-07-2004 10:58
+# Auto generated 31-07-2004 23:25
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '3.6.1-0.0.1',
-	'PHP_version' => '0.0.4-0.0.4',
+	'TYPO3_version' => '3.6.2-0.0.2',
+	'PHP_version' => '0.0.5-0.0.5',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -28,15 +28,15 @@ $EM_CONF[$_EXTKEY] = Array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author' => 'Stig Nørgaard Jepsen',
-	'author_email' => 'stig@8620.dk',
+	'author' => 'Rupert Germann',
+	'author_email' => 'rupi@gmx.li',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.2.3',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:49:{s:9:"Thumbs.db";s:4:"e98f";s:20:"class.ext_update.php";s:4:"49e6";s:12:"ext_icon.gif";s:4:"5e2a";s:15:"ext_icon__h.gif";s:4:"e1bc";s:16:"ext_icon__ht.gif";s:4:"dbd6";s:17:"ext_icon__htu.gif";s:4:"7975";s:16:"ext_icon__hu.gif";s:4:"865e";s:15:"ext_icon__t.gif";s:4:"ef88";s:16:"ext_icon__tu.gif";s:4:"22a2";s:15:"ext_icon__u.gif";s:4:"0fd4";s:15:"ext_icon__x.gif";s:4:"ede5";s:14:"ext_tables.php";s:4:"9a49";s:14:"ext_tables.sql";s:4:"254f";s:28:"ext_typoscript_constants.txt";s:4:"7f17";s:24:"ext_typoscript_setup.txt";s:4:"8678";s:15:"flexform_ds.xml";s:4:"4b15";s:13:"locallang.php";s:4:"35cf";s:24:"locallang_csh_ttnews.php";s:4:"4560";s:25:"locallang_csh_ttnewsc.php";s:4:"5e3d";s:17:"locallang_tca.php";s:4:"123b";s:7:"tca.php";s:4:"302a";s:11:"CVS/Entries";s:4:"72c2";s:17:"CVS/Entries.Extra";s:4:"8380";s:21:"CVS/Entries.Extra.Old";s:4:"7dbd";s:15:"CVS/Entries.Old";s:4:"3ab8";s:14:"CVS/Repository";s:4:"2a14";s:8:"CVS/Root";s:4:"5e5e";s:15:"doc/CVS/Entries";s:4:"5917";s:21:"doc/CVS/Entries.Extra";s:4:"3735";s:25:"doc/CVS/Entries.Extra.Old";s:4:"3735";s:19:"doc/CVS/Entries.Old";s:4:"11bf";s:18:"doc/CVS/Repository";s:4:"925e";s:12:"doc/CVS/Root";s:4:"5e5e";s:17:"pi/News conf1.gif";s:4:"cbd5";s:12:"pi/Thumbs.db";s:4:"53b6";s:13:"pi/ce_wiz.gif";s:4:"9b9b";s:13:"pi/ce_wiz.psd";s:4:"8e4f";s:22:"pi/class.tx_ttnews.php";s:4:"f4c3";s:30:"pi/class.tx_ttnews_wizicon.php";s:4:"cc64";s:10:"pi/new.gif";s:4:"7f00";s:17:"pi/news_help.tmpl";s:4:"bec6";s:17:"pi/news_help1.gif";s:4:"d8fb";s:21:"pi/news_template.tmpl";s:4:"2368";s:14:"pi/CVS/Entries";s:4:"6d93";s:20:"pi/CVS/Entries.Extra";s:4:"9cae";s:24:"pi/CVS/Entries.Extra.Old";s:4:"63e1";s:18:"pi/CVS/Entries.Old";s:4:"1193";s:17:"pi/CVS/Repository";s:4:"be88";s:11:"pi/CVS/Root";s:4:"5e5e";}',
+	'version' => '1.4.1',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:30:{s:20:"class.ext_update.php";s:4:"55cd";s:12:"ext_icon.gif";s:4:"5e2a";s:15:"ext_icon__h.gif";s:4:"e1bc";s:16:"ext_icon__ht.gif";s:4:"dbd6";s:17:"ext_icon__htu.gif";s:4:"7975";s:16:"ext_icon__hu.gif";s:4:"865e";s:15:"ext_icon__t.gif";s:4:"ef88";s:16:"ext_icon__tu.gif";s:4:"22a2";s:15:"ext_icon__u.gif";s:4:"0fd4";s:15:"ext_icon__x.gif";s:4:"ede5";s:14:"ext_tables.php";s:4:"048d";s:14:"ext_tables.sql";s:4:"254f";s:28:"ext_typoscript_constants.txt";s:4:"bd0d";s:24:"ext_typoscript_setup.txt";s:4:"bdf0";s:15:"flexform_ds.xml";s:4:"f08d";s:13:"locallang.php";s:4:"65c6";s:24:"locallang_csh_ttnews.php";s:4:"b209";s:25:"locallang_csh_ttnewsc.php";s:4:"639b";s:17:"locallang_tca.php";s:4:"0002";s:7:"tca.php";s:4:"72b2";s:13:"pi/ce_wiz.gif";s:4:"9b9b";s:22:"pi/class.tx_ttnews.php";s:4:"735f";s:30:"pi/class.tx_ttnews_wizicon.php";s:4:"baf1";s:16:"pi/locallang.php";s:4:"4bd3";s:10:"pi/new.gif";s:4:"7f00";s:17:"pi/news_conf1.png";s:4:"a914";s:17:"pi/news_help.tmpl";s:4:"4fb4";s:17:"pi/news_help1.gif";s:4:"d8fb";s:21:"pi/news_template.tmpl";s:4:"ce80";s:14:"doc/manual.sxw";s:4:"b884";}',
 );
 
 ?>
