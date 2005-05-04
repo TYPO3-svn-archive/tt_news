f1020786069201555114493ab809bf4eca96a4c9
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-05-04 09:04:22 +0000

    - updated comments

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
-# extends the wizard_add script to set default values in new created records
+// extends the "wizard_add" script to set default values in new created records (used for: "create new subcategory" in hte category record)
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/wizard_add.php']=t3lib_extMgm::extPath($_EXTKEY).'class.ux_SC_wizard_add.php';
 
 
@@ -10,6 +10,5 @@ $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/wizard_add.php']=t3lib_extMgm::ext
 // This check will be disabled until 'options.useListOfAllowedItems' (user/group TSconfig) is set to a value.
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:tt_news/class.tx_ttnews_tcemain.php:tx_ttnews_tcemain';
 
-#$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['calcPerms'][] = 'EXT:tt_news/class.tx_ttnews_userauthgroup.php:tx_ttnews_userauthgroup->calcPerms';
 
 ?>
