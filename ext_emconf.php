1c176b4e248c34df49701cf2ae93226955b3fb03
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-15 19:24:28 +0000

    Raise required TYPO3 version to 4.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'cms' => '',
 			'php' => '5.0.0-5.3.99',
-			'typo3' => '4.1.0-4.6.99',
+			'typo3' => '4.3.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
